bce645ed2dacde49486924a1c7dc8abb5e928dda
cksdud32 <agcksdud32@gmail.com>
2025-11-04 11:53:44 +0900

    링크 바로가기 추가

---
+++ loadHeaderFooter.js
@@ -0,0 +1,17 @@
+function loadHTML(url, elementId) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! 상태 코드: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error(`${elementId} 불러오기 실패:`, error));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadHTML('link.html', 'link');
+});
