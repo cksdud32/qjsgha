8a4ece93a83c12bc16f60ac6eaeb8598be18d216
cksdud32 <agcksdud32@gmail.com>
2025-11-13 11:31:48 +0900

    위치 이동 및 재설정

--- loadHeaderFooter.js
+++ loadHeaderFooter.js
@@ -12,8 +12,10 @@ function loadHTML(url, elementId) {
         .catch(error => console.error(`${elementId} 불러오기 실패:`, error));
 }
 
+const pathPrefix = iocation.pathname.includes('/html/')?'../html':'html/';
+
 document.addEventListener('DOMContentLoaded', () => {
-    loadHTML('link.html', 'link');
-    loadHTML('Shor.html', 'Shor');
-    loadHTML('footer.html', 'footer');
+    loadHTML('${pathPrefix}link.html', 'link');
+    loadHTML('${pathPrefix}Shor.html', 'Shor');
+    loadHTML('${pathPrefix}footer.html', 'footer');
 });
